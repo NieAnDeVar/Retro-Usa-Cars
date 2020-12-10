7d7337520b9e8c1c9c9e9c215c804dc4a43223cc
Niyaz <nieandevar@mail.ru>
2020-12-10 22:31:30 +0300

    index

---
+++ RetroUsaCars/RetroUsaCars/Pages/Log in.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RetroUsaCars.Pages
+{
+    public class Log_in : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
